b5298e6752512e6c6080f93cef95a068506c8000
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-02-22 11:31:17 -0300

    Add files via upload

---
+++ ola mundo.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	
+	int idade = 10;
+	float peso = 80.550;
+	char sexo = 'F';
+	char nome[200] = "Marta";
+	
+	printf("Idade: %i \n", idade);//%i = int
+	printf("Peso: %f \n", peso);//%f = float
+	printf("Sexo: %c \n", sexo);//%c = char
+	printf("Nome: %s \n", nome);//%s = string
+	
+	return 0;
+	 
+}
+
+
+	
+	
