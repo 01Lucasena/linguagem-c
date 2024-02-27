553f3f3ef876f1540856b26c03ab0ee202e71724
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-02-27 09:58:48 -0300

    Add files via upload

---
+++ sucessor-antecessor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	//Declarar variaveis
+	int numero, antecessor, sucessor;
+	
+	//Solicitar dados ao usuário
+	printf("Digite um numero: ");
+	scanf("%i", &numero);
+	
+	
+	//Mostrar número sucessor e antecessor
+	sucessor = numero + 1;
+	antecessor = numero - 1;
+	printf("Sucessor: %i \n", sucessor);
+	printf("Antecessor: %i \n", antecessor);
+	
+	return 0;
+}
