d980b974f0cde06e47cdd832d807e3eeb64ed404
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-02-28 09:18:25 -0300

    Update reajuste.c

--- reajuste.c
+++ reajuste.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void cabecalho(){
+	
+	system("cls||clear");
+	printf("\n===SENAI===\n");
+}
 
 int main(){
-	//declarar variáveis
+	
+	//Declarar variÃ¡veis
+	
 	float valor, reajuste10;
 	
-	//Solicitar dados ao usuário
+	//Solicitar dados ao usuÃ¡rio
+
+	cabecalho();
 	printf("Digite um valor: ");
 	scanf("%f", &valor);
+	system("cls||clear");
 	
 	//Calcular reajuste de 10%
+	
 	reajuste10 = valor - (valor * 0,10);
 	
 	//Exibir valor com reajuste
-	printf("\n>>>EXIBINDO DADOS<<<\n");
+	
+	printf("\n===EXIBINDO DADOS===\n");
 	printf("Valor: R$ %.2f\n", valor);
 	printf("Valor com reajuste de 10%: R$ %.2f\n", reajuste10);
-	
-	
 	
 	return 0;
 }
