072f69061c8fa87c4e0e969ad3864c4b939f9496
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-02-28 09:10:39 -0300

    Update par-impar.c

--- par-impar.c
+++ par-impar.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void cabecalho(){
+	
+	system("cls||clear");
+	printf("\n===SENAI===\n");
+}
 
 int main(){
 	//Declarar variáveis
+	
 	int numero;
 	
 	//Solicitar dados
+	
+	cabecalho();
 	printf("Digite um numero: ");
 	scanf("%i", &numero);
+	system("cls||clear");
 	
 	//Verificar se é par ou ímpar
-	printf("\n>>>EXIBINDO DADOS<<<\n");
+	
+	printf("\n===EXIBINDO RESULTADOS===\n");
+	
 	if(numero%2==0){
 		printf("Numero par.");
 	}else{
