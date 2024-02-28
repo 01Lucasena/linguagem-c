e8509c824592b4b59a0773604884ed9b0a1cbc24
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-02-28 10:59:31 -0300

    Update sucessor-antecessor.c

--- sucessor-antecessor.c
+++ sucessor-antecessor.c
@@ -5,6 +5,7 @@ void cabecalho(){
 	
 	system("cls||clear");
 	printf("\n===SENAI===\n");
+	fflush(stdin); 
 
 }
 
@@ -18,7 +19,7 @@ int main(){
 
 	cabecalho();
 	printf("Digite um numero: ");
-	scanf("%i", &numero);
+	scanf("%i",&numero);
 	system("cls||clear");
 	
 	//Verificar sucessor e antecessor
@@ -29,8 +30,8 @@ int main(){
 	//Exibindo dados ao usuário 
 	
 	printf("\n===EXIBINDO RESULTADOS===\n");
-	printf("Sucessor: %i \n", sucessor);
-	printf("Antecessor: %i \n", antecessor);
+	printf("Sucessor: %i \n",sucessor);
+	printf("Antecessor: %i \n",antecessor);
 	
 	return 0;
 }
