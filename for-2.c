9c29efb18711d53fd1c71d688b9287d0154f3932
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-04-06 11:07:15 -0300

    Update and rename média for.c to for-2.c

--- for-2.c
+++ for-2.c
@@ -14,24 +14,24 @@ int main(){
 	
 	setlocale(LC_ALL,"");
 	
-	//Declarar vari·veis
+	//Declarar vari√°veis
 	
 	float nota=0, media=0, soma=0;
 	int i;
 	
-	//Solicitar dados ao usu·rio
+	//Solicitar dados ao usu√°rio
 	
 	for(i=1;i<=4;i++){
 		
 		cabecalho();
-		printf("Digite a %d™ nota: ", i);
+		printf("Digite a %d¬™ nota: ", i);
 		scanf("%f",&nota);
 		soma = soma + nota;
 		system("cls||clear");
 		
 	}
 	media = soma / (i-1);
-	printf("\nMÈdia %.2f: ",media);
+	printf("\nM√©dia %.2f: ",media);
 	
 	return 0;
 
