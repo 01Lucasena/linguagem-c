54affddaa2e8842cc118f5ece4367e0098377862
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-04-06 09:50:49 -0300

    Update vetores-1.c

--- vetores-1.c
+++ vetores-1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <locale.h>
+
+#define SIZE 5
 
 int main(){
 	
-	int i, vetor[5];
+	int i, vetor[SIZE];
 	
 	printf("Digite os elementos do vetor \n");
 	
-	for(i = 0; i < 5; i ++){
+	for(i = 0; i < SIZE; i ++){
 		
 		printf("Elemento %d: ", i+1);
 		scanf("%d",&vetor[i]);
@@ -15,7 +18,7 @@ int main(){
 	
 	printf("\n===EXIBINDO RESULTADOS===\n");
 	
-	for(i = 0; i < 5; i ++){
+	for(i = 0; i < SIZE; i ++){
 		
 		
 			printf("Elemento %d: %d\n", i+1, vetor[i]);
