016cda87faebaddafebf4fb3d7f792a3485f55dd
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-04-09 13:55:17 -0300

    Update vetores-7.c

--- vetores-7.c
+++ vetores-7.c
@@ -7,14 +7,14 @@ int main(){
 	
 	setlocale(LC_ALL,"");
 	
-	int numero[QUANTIDADE_DE_NUMEROS], i, j, contadorPares=0, contadorImpares=0, contadorPositivos=0; 
+	int numero[QUANTIDADE_DE_NUMEROS], i, contadorPares=0, contadorImpares=0, contadorPositivos=0; 
 	int contadorNegativos=0, maiorNumero=0, menorNumero=999, somaPares=0, somaImpares=0;
 	int somaGeral=0, numeroInverso[QUANTIDADE_DE_NUMEROS];
 	float mediaPares, mediaImpares, mediaGeral;
 	
 	for(i=0;i<QUANTIDADE_DE_NUMEROS;i++){
 		
-		printf("\nDigite o %dº número: ",i+1);
+		printf("\nDigite o %dÂº nÃºmero: ",i+1);
 		scanf("%d",&numero[i]);
 		
 		if(numero[i]%2 == 0){
@@ -60,23 +60,23 @@ int main(){
 	mediaGeral = somaGeral / (float)QUANTIDADE_DE_NUMEROS;
 	
 	printf("\n===EXIBINDO RESULTADOS===\n");
-	printf("\nQuantidade de números pares: %d",contadorPares);
-	printf("\nQuantidade de números ímpares: %d",contadorImpares);
-	printf("\nQuantidade de números positivos: %d",contadorPositivos);
-	printf("\nQuantidade de números negativos: %d",contadorNegativos);
-	printf("\nQuantidade de números inseridos: %d",QUANTIDADE_DE_NUMEROS);
-	printf("\nMaior número: %d",maiorNumero);
-	printf("\nMenor número: %d",menorNumero);
-	printf("\nMedia de números pares: %.1f",mediaPares);
-	printf("\nMedia de números ímpares: %.1f",mediaImpares);
-	printf("\nMedia de todos os números inseridos: %.1f\n",mediaGeral);
+	printf("\nQuantidade de nÃºmeros pares: %d",contadorPares);
+	printf("\nQuantidade de nÃºmeros Ã­mpares: %d",contadorImpares);
+	printf("\nQuantidade de nÃºmeros positivos: %d",contadorPositivos);
+	printf("\nQuantidade de nÃºmeros negativos: %d",contadorNegativos);
+	printf("\nQuantidade de nÃºmeros inseridos: %d",QUANTIDADE_DE_NUMEROS);
+	printf("\nMaior nÃºmero: %d",maiorNumero);
+	printf("\nMenor nÃºmero: %d",menorNumero);
+	printf("\nMedia de nÃºmeros pares: %.1f",mediaPares);
+	printf("\nMedia de nÃºmeros Ã­mpares: %.1f",mediaImpares);
+	printf("\nMedia de todos os nÃºmeros inseridos: %.1f\n",mediaGeral);
 	
-	printf("\nNúmeros inseridos em ordem inversa: \n");
+	printf("\nNÃºmeros inseridos em ordem inversa: \n");
 	
 	for(i=0;i<QUANTIDADE_DE_NUMEROS;i++){
 
 		numeroInverso[i] = numero[QUANTIDADE_DE_NUMEROS-i-1];
-		printf("%dº Número: %d\n",i+1,numeroInverso[i]);
+		printf("%dÂº NÃºmero: %d\n",i+1,numeroInverso[i]);
 		
 	}
 	
