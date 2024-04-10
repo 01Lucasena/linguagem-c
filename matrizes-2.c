d8815ffd6c47bb192d5cd009330d00f1f423d249
01Lucasena <159453926+01Lucasena@users.noreply.github.com>
2024-04-10 08:56:49 -0300

    Update matrizes-2.c

--- matrizes-2.c
+++ matrizes-2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
-#include <stdlib.h>
+#include <string.h> 
 
 #define QUANTIDADE_DE_DISCIPLINAS 3
 #define QUANTIDADE_DE_NOTAS 2
@@ -9,46 +9,64 @@ int main(){
 	
 	setlocale(LC_ALL,"");
 	
-	char disciplina[QUANTIDADE_DE_DISCIPLINAS][200];
-	float notas[QUANTIDADE_DE_DISCIPLINAS][QUANTIDADE_DE_NOTAS], soma=0, media[QUANTIDADE_DE_DISCIPLINAS];
 	int i, j;
+	float nota[QUANTIDADE_DE_DISCIPLINAS][QUANTIDADE_DE_NOTAS], media[QUANTIDADE_DE_DISCIPLINAS], soma=0;
+	char disciplina[QUANTIDADE_DE_DISCIPLINAS][200];
 	
-	
-	for(i=0;i<QUANTIDADE_DE_DISCIPLINAS;i++){
+	for(i = 0; i < QUANTIDADE_DE_DISCIPLINAS; i++){
 		
-		printf("\nDigite o nome da disciplina: ");
-		scanf("%s",&disciplina[i]);
+		printf("\nDigite o nome da %dª disciplina: ",i+1);
+		gets(disciplina[i]);
 		
 		fflush(stdin);
 		
-		for(j=0;j<QUANTIDADE_DE_NOTAS;j++){
+		for(j = 0; j < QUANTIDADE_DE_NOTAS; j++){
 			
 			printf("\nDigite a %dª nota: ",j+1);
-			scanf("%f",&notas[i][j]);
+			scanf("%f",&nota[i][j]);
 			
-			soma += notas[i][j];
+			soma += nota[i][j];
+			
 		}
 		
 		media[i] = soma / j;
 		soma = 0;
 	}
 	
+
+	
 	printf("\n===EXIBINDO RESULTADOS===\n");
 	
-	for(i=0;i<QUANTIDADE_DE_DISCIPLINAS;i++){
+	for(i = 0; i < QUANTIDADE_DE_DISCIPLINAS; i++){
 		
-		printf("\nDisciplina %d: %s\n",i+1,disciplina[i]);
+		printf("\n%dª Disciplina: %s\n",i+1,disciplina[i]);
 		
-		
-		for(j=0;j<QUANTIDADE_DE_NOTAS;j++){
+		for(j = 0; j < QUANTIDADE_DE_NOTAS; j++){
 			
-			printf("\n%dª Nota: %.1f\n",j+1,notas[i][j]);
-			
+			printf("\n%dª Nota: %.1f\n",j+1,nota[i][j]);
+		
 		}
 		
 		printf("\nMédia: %.1f\n",media[i]);
+		
+		if(media[i] >= 7){
+			
+			printf("\nAprovado\n");
+			
+		}else if(media[i] >= 5 && media[i] < 7){
+		
+			printf("\nRecuperação\n");
+			
+		}else{
+			
+			printf("\nReprovado\n");
+		
+		}
+		
+		printf("\n==========================\n");
 	
 	}
 	
 	return 0;
+	
 }
